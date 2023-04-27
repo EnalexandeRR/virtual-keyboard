a3dfaa3c7cac35ad2c54a7efb213d1e710b86d15
Alexandr Yenussov <a.yenussov@gmail.com>
2023-04-27 13:37:11 +0600

    feat: add test values to keys object

---
+++ js/binds.js
@@ -0,0 +1,36 @@
+const props = {
+  en: {
+    Backquote: {
+      caseDown: '`',
+      caseUp: '~',
+      caps: '`',
+      shiftCaps: '~',
+    },
+    Digit1: {
+      caseDown: '1',
+      caseUp: '!',
+      caps: '1',
+      shiftCaps: '!',
+    },
+    Digit2: {
+      caseDown: '2',
+      caseUp: '@',
+      caps: '2',
+      shiftCaps: '@',
+    },
+    Digit3: {
+      caseDown: '3',
+      caseUp: '#',
+      caps: '3',
+      shiftCaps: '#',
+    },
+    Digit4: {
+      caseDown: '4',
+      caseUp: '$',
+      caps: '4',
+      shiftCaps: '$',
+    },
+  },
+};
+
+export default props;
